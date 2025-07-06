7278b4170af602006d80b6615ea5a8bfcdf8c9c3
spynalcode <spynalcode@gmail.com>
2025-07-06 02:07:49 +0100

    added more files

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ import { Footer } from './components/footer.js';
 import { HomePage } from './pages/home.js';
 import { Cart } from './components/cart.js';
 
+// js/app.js
+document.addEventListener('DOMContentLoaded', () => {
+  // Navbar will render automatically from HTML
+  console.log('Website loaded!');
+});
 class App {
   constructor() {
     this.cart = new Cart();
